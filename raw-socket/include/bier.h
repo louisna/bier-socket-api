236b87c31730177aa0e9bf04475d26afdfc328bc
Louis Navarre <louisnavarre@hotmail.com>
2022-04-08 10:46:12 +0200

    Add documentation

--- raw-socket/include/bier.h
+++ raw-socket/include/bier.h
@@ -49,34 +49,50 @@
         d8[5] |= (bsl << 4);        \
     }
 
+/**
+ * @brief Bits operations on the bitstring
+ */
 typedef enum
 {
     bitwise_u64_and_not,
     bitwise_u64_and
 } bitstring_operation;
 
+/**
+ * @brief Representation of an entry of the BIER Forwarding Table
+ */
 typedef struct
 {
-    uint32_t bfr_id;
-    uint64_t *forwarding_bitmask;
-    uint32_t bitstring_length;
-    int32_t bfr_nei; // BIER Forwarding Router Neighbour
-    struct sockaddr_in6 bfr_nei_addr;
+    uint32_t bfr_id; // BFR-ID of the neighbour router in the network
+    uint64_t *forwarding_bitmask; // The bitmask of this BFR entry
+    uint32_t bitstring_length; // Length of `forwarding_bitmask` in bits
+    int32_t bfr_nei; // BIER Forwarding Router Neighbour - Decrepated
+    struct sockaddr_in6 bfr_nei_addr; // Socket address to reach the BFR neighbors of this entry
 } bier_bft_entry_t;
 
+/**
+ * @brief Representation of the state of a BIER Forwarding Router
+ */
 typedef struct
 {
-    int local_bfr_id;
-    struct sockaddr_in6 local;
-    int nb_bft_entry;
-    uint32_t bitstring_length; // In bits
-    int socket;
-    bier_bft_entry_t **bft;
+    int local_bfr_id; // BFR-ID of the router in the network
+    struct sockaddr_in6 local; // Socket address with the loopback address of the router
+    int nb_bft_entry; // Number of entries in the BIER Forwarding Table
+    uint32_t bitstring_length; // Represents the "BSL" in bits
+    int socket; // Socket to send and receive packets
+    bier_bft_entry_t **bft; // Table of length `nb_bft_entry` containing all entries of the BIER Forwarding Table
 } bier_internal_t;
 
+/**
+ * @brief Local processing function when the router receives a packet belonging to itself.
+ */
 typedef struct
 {
-    void *args;
+    void *args; // Additional arguments to provide to the `local_processing_function` function
+    // Function that will be executed when the router receives a packet belonging to it.
+    // `bier_packet` is the buffer containing the packet starting at the BIER header. The packet is of length `packet_length`
+    // and the BIER header is of length `bier_header_length`. As a result, the content of the BIER packet is of length `packet_length` - `bier_header_length`.
+    // The function takes an additional pointer with arguments that the application can provide to the function.
     void (*local_processing_function)(const uint8_t *bier_packet, const uint32_t packet_length, const uint32_t bier_header_length, void *args);
 } bier_local_processing_t;
 
@@ -107,6 +123,12 @@ void free_bier_bft(bier_internal_t *bft);
  * @return int error indication state
  */
 int bier_processing(uint8_t *buffer, size_t buffer_length, bier_internal_t *bft, bier_local_processing_t *bier_local_processing);
+
+/**
+ * @brief Prints to the standard output the content of the BIER Forwarding table `bft`.
+ * 
+ * @param bft the BIER Forwarding Table to display.
+ */
 void print_bft(bier_internal_t *bft);
 
 #endif // BIER_H
