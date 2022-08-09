3ccd4797f11f4a93b640978cf36abeee9eba5fd0
Louis Navarre <louisnavarre@hotmail.com>
2022-08-09 11:09:49 +0200

    Forgot to uncomment bind_bier

--- raw-socket/src/public_bier.c
+++ raw-socket/src/public_bier.c
@@ -95,29 +95,29 @@ ssize_t recvfrom_bier(int socket, void *buf, size_t len,
     return nb_read_return;
 }
 
-// int bind_bier(int socket, const struct sockaddr *bier_sock_path, bier_bind_t *bind_to) {
-//     size_t qcbor_length = sizeof(bier_bind_t) + 200; // Make room for other information encoding
-//     UsefulBuf_MAKE_STACK_UB(Buffer, qcbor_length);
+int bind_bier(int socket, const struct sockaddr *bier_sock_path, bier_bind_t *bind_to) {
+    size_t qcbor_length = sizeof(bier_bind_t) + 200; // Make room for other information encoding
+    UsefulBuf_MAKE_STACK_UB(Buffer, qcbor_length);
 
-//     QCBOREncodeContext ctx;
-//     QCBOREncode_Init(&ctx, Buffer);
-//     QCBOREncode_OpenMap(&ctx);
+    QCBOREncodeContext ctx;
+    QCBOREncode_Init(&ctx, Buffer);
+    QCBOREncode_OpenMap(&ctx);
 
-//     // Indicator
-//     QCBOREncode_AddInt64ToMap(&ctx, "type", BIND);
+    // Indicator
+    QCBOREncode_AddInt64ToMap(&ctx, "type", BIND);
 
-//     UsefulBufC unix_path_buf = {bind_to->unix_path, NAME_MAX};
-//     QCBOREncode_AddBytesToMap(&ctx, "unix_path", unix_path_buf);
-//     UsefulBufC mc_addr_buf = {&bind_to->mc_addr, sizeof(struct in6_addr)};
-//     QCBOREncode_AddBytesToMap(&ctx, "mc_addr", mc_addr_buf);
-//     QCBOREncode_CloseMap(&ctx);
+    UsefulBufC unix_path_buf = {bind_to->unix_path, NAME_MAX};
+    QCBOREncode_AddBytesToMap(&ctx, "unix_path", unix_path_buf);
+    UsefulBufC mc_addr_buf = {&bind_to->mc_addr, sizeof(struct in6_addr)};
+    QCBOREncode_AddBytesToMap(&ctx, "mc_addr", mc_addr_buf);
+    QCBOREncode_CloseMap(&ctx);
 
-//     UsefulBufC EncodedCBOR;
-//     QCBORError uErr;
-//     uErr = QCBOREncode_Finish(&ctx, &EncodedCBOR);
-//     if (uErr != QCBOR_SUCCESS) {
-//         return -1;
-//     }
+    UsefulBufC EncodedCBOR;
+    QCBORError uErr;
+    uErr = QCBOREncode_Finish(&ctx, &EncodedCBOR);
+    if (uErr != QCBOR_SUCCESS) {
+        return -1;
+    }
 
-//     return 0; // Success
-// }
+    return 0; // Success
+}
